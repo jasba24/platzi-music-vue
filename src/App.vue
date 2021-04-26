66f642be63d9d93a566138bbfe44657172c811a9
jasba24 <johanbuitrago342@gmail.com>
2021-04-26 18:51:11 -0500

    add the ui and logic for the app

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<section class="section">
+		<nav class="navbar has-shadow">
+			<div class="container">
+				<input
+					type="text"
+					placeholder="Buscar canciones"
+					class="input is-large"
+					v-model="searchQuery"
+				/>
+				<a @click.prevent="search" href="" class="button is-info is-large"
+					>Buscar</a
+				>
+				<a href="" class="button is-danger is-large">&times;</a>
+			</div>
+		</nav>
+		<div class="container">
+			<div class="colums">
+				<div class="column" v-for="t in tracks">
+					{{ t.name }} - {{ t.artist }}
+				</div>
+			</div>
+		</div>
+				<p><small>{{searchMessage}}</small></p>
+	</section>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+const tracks = [
+	{
+		name: "Muchacha",
+		artist: "Luis Alberto Spinetta",
+	},
+	{
+		name: "Hoy aca en el baile",
+		artist: "El pepo",
+	},
+	{
+		name: "I was made for loving yo",
+		artist: "Kiss",
+	},
+]
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	name: "App",
+
+	data() {
+		return {
+			searchQuery: "",
+			tracks: [],
+		}
+	},
+
+	computed: {
+		searchMessage() {
+			return `Encontrados: ${this.tracks.length}`
+		}
+	},
+
+	methods: {
+		search() {
+			this.tracks = tracks
+		},
+	},
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "scss/main.scss";
 </style>
