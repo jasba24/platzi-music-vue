df7368ac0b50bbf418f5d80231b1eb580b406c4e
jasba24 <johanbuitrago342@gmail.com>
2021-05-04 14:30:53 -0500

    add dist folder to repository to deploy in vercel

---
+++ dist/js/chunk-2d22d746.186043d7.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(n,t,c){"use strict";c.r(t);var e=c("7a23"),s={class:"container"},o=Object(e["h"])("h1",{class:"title"}," Nosotros ",-1);function r(n,t){return Object(e["q"])(),Object(e["e"])("div",s,[o])}const a={};a.render=r;t["default"]=a}}]);
+//# sourceMappingURL=chunk-2d22d746.186043d7.js.map
